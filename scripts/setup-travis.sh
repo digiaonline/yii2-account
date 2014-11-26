4fba8cd4d5d71c412cc0628ef7b7c3c3c9fb2741
Christoffer Niska <christofferniska@gmail.com>
2014-11-26 13:22:43 -0800

    Update setup-travis.sh

--- scripts/setup-travis.sh
+++ scripts/setup-travis.sh
@@ -4,10 +4,8 @@
 mysql -e 'CREATE DATABASE account_test CHARACTER SET utf8;'
 
 # configure codeception
-export DB_HOST=127.0.0.1 DB_USER=root DB_PASS=''
 erb tests/app/config/env.php.erb > tests/app/config/env.php
 erb tests/codeception.yml.erb > tests/codeception.yml
-export BASE_URL='http://localhost:8000/'
 erb tests/codeception/acceptance.suite.yml.erb > tests/codeception/acceptance.suite.yml
 
 # install dependencies through composer
