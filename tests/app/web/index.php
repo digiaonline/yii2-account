ded303915d8d72a57d981ab3fa223776dbefb9ef
Christoffer Niska <christofferniska@gmail.com>
2014-11-26 14:23:26 -0800

    Update index.php

--- tests/app/web/index.php
+++ tests/app/web/index.php
@@ -7,13 +7,10 @@ defined('YII_ENV') or define('YII_ENV', 'test');
 
 defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', dirname(dirname(dirname(__DIR__))));
 
-require(YII_APP_BASE_PATH . '/tests/codeception/c3.php');
-
 require(YII_APP_BASE_PATH . '/vendor/autoload.php');
 require(YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php');
+require(YII_APP_BASE_PATH . '/tests/codeception/c3.php');
 
 $config = require(dirname(__DIR__) . '/config/acceptance.php');
-
-define('MY_APP_STARTED', true);
 
 (new yii\web\Application($config))->run();
