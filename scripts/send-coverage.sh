c9c6de6664c296fe10f61f47390f6954f06b4a0c
Christoffer Niska <christofferniska@gmail.com>
2014-11-26 13:34:22 -0800

    Update .travis.yml and send-coverage.sh

--- scripts/send-coverage.sh
+++ scripts/send-coverage.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+mv tests/codeception/_output/coverage.xml build/logs/clover.xml
 CODECLIMATE_REPO_TOKEN=848604f9639e4a3fcffe8a7485db17a8746ae724ecb5873b0ec571ad37267506 /vagrant/vendor/bin/test-reporter
