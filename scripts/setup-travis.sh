92469c5f4edd912d65a595f9d98875e460b3ae42
Christoffer Niska <christofferniska@gmail.com>
2014-11-28 08:13:12 -0800

    Added a missing composer dependency to setup-travis.sh

--- scripts/setup-travis.sh
+++ scripts/setup-travis.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# create the database
-mysql -e 'CREATE DATABASE account_test CHARACTER SET utf8;'
-
 # configure codeception
 erb tests/app/config/env.php.erb > tests/app/config/env.php
 erb tests/codeception.yml.erb > tests/codeception.yml
 erb tests/codeception/acceptance.suite.yml.erb > tests/codeception/acceptance.suite.yml
 
 # install dependencies through composer
-composer self-update
+composer self-update && composer --version
+composer global require "fxp/composer-asset-plugin:1.0.0-beta3"
 composer install --no-interaction --prefer-source
 
 # start the web server
@@ -17,3 +15,6 @@ php -S localhost:8000 -t tests/app/web/ > /dev/null 2>&1 &
 
 # install codeception
 wget http://codeception.com/codecept.phar
+
+# create the database
+mysql -e 'CREATE DATABASE account_test CHARACTER SET utf8;'
